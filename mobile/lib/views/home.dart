c4c74f4fd2ac765aef36ce8012323a66681a6781
emmanuellorey@gmail.com <LOREYLOLIM>
2023-03-31 13:10:26 +0300

    new commit

--- mobile/lib/views/home.dart
+++ mobile/lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,8 +11,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    // final box = GetStorage();
+    // var token = box.read('token');
+
     return Scaffold(
       body: Center(
+        // child: Text(token),
         child: Text('Home Page'),
       ),
     );
